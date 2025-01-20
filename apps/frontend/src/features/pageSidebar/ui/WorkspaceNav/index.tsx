5c8d89f9e117b78168192d825143b866e382dc44
pkh0106 <rudgml742@gmail.com>
2025-01-20 14:16:12 +0900

    refactor : TopNavView Nav component React.memo 적용
    - 페이지 목록을 열고 닫을 때마다 고정 컴포넌트들이 리렌더링되는 문제 개선
    
    Co-authored-by: baegyeong <baegyeong@users.noreply.github.com>

--- apps/frontend/src/features/pageSidebar/ui/WorkspaceNav/index.tsx
+++ apps/frontend/src/features/pageSidebar/ui/WorkspaceNav/index.tsx
@@ -1,11 +1,15 @@
+import { memo } from "react";
+
 interface WorkspaceNavProps {
   title: string;
 }
 
-export function WorkspaceNav({ title }: WorkspaceNavProps) {
+function WorkspaceNavComponent({ title }: WorkspaceNavProps) {
   return (
     <div className="flex flex-row items-center justify-center gap-2">
       <h1 className="text-md font-semibold">{title}</h1>
     </div>
   );
 }
+
+export const WorkspaceNav = memo(WorkspaceNavComponent);
