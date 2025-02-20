1a48d49e2cf84ccd1f56603721cc06dc751710aa
pkh0106 <rudgml742@gmail.com>
2025-02-20 17:44:40 +0900

    feat: RAG 답안 react-markdown 적용

--- apps/frontend/src/features/AI/ui/QnA/index.tsx
+++ apps/frontend/src/features/AI/ui/QnA/index.tsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 interface QnAType {
   question: string;
   answer: string;
@@ -9,7 +11,9 @@ export function QnA({ question, answer }: QnAType) {
       <div className="mb-1 w-full rounded-md border-[1px] border-[#d0d9e0] bg-[#f5f6fa] px-3 py-2 font-bold">
         {question}
       </div>
-      <div className="min-h-[100px] p-2 font-light">{answer}</div>
+      <div className="min-h-[100px] p-2">
+        <ReactMarkdown>{answer}</ReactMarkdown>
+      </div>
     </div>
   );
 }
