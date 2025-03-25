280e4c839462e629cd5ac1772bcdc50d8e704632
pkh0106 <rudgml742@gmail.com>
2025-03-25 11:28:44 +0900

    feat: 뒤로 가기 시 화면 축소 기능 구현
    - 전체 화면 변경 시 뒤로 가기 스택에 동일 url 추가

--- apps/frontend/src/widgets/EditorView/model/useEditorView.ts
+++ apps/frontend/src/widgets/EditorView/model/useEditorView.ts
@@ -8,10 +8,27 @@ import { useEdtorConnection } from "@/features/editor/model/useEditorConnection"
 
 export const useEditorView = () => {
   const { currentPage } = usePageStore();
-  const { isPanelOpen, isMaximized, setIsPanelOpen } = useEditorStore();
+  const { isPanelOpen, isMaximized, setIsPanelOpen, toggleMaximized } =
+    useEditorStore();
   useEdtorConnection(currentPage);
   const { editor } = useConnectionStore();
   const { users } = useUserStore();
+
+  useEffect(() => {
+    if (isMaximized) {
+      history.pushState(null, "");
+    }
+
+    const handlePopState = () => {
+      toggleMaximized();
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [isMaximized]);
 
   useEffect(() => {
     setIsPanelOpen(!!currentPage);
